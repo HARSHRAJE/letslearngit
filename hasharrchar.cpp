99d8f56875fa3da98abde16cebac196584fae8a2
Harsh Raje <vasamama72@gmail.com>
2024-01-31 07:27:07 +0530

    added hasharr.cpp

---
+++ hasharrchar.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n=5;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    //hash array
+    int max=6;
+    int hashh[max]={0};
+    for(int i=0;i<n;i++){
+        hashh[arr[i]]++;
+    }
+    for(int i=0;i<n;i++){
+       cout<<hashh[arr[i]]<<" ";
+    }
+    return 0;
+    
+}
